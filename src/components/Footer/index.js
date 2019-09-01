3956f164fe170b99cf8ba393e913920958d370c8
mist <505168746@qq.com>
2019-09-01 16:29:10 +0800

    eject-berore

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './index'
+
+export default class Footer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                This Footer 
+            </div>
+        );
+    }
+
+}
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class NavLeft extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return (
+            <div style={{backgroundColor:'red'}}> 
+                This NavLeft 
+            </div>
+        );
+    }
+
+}
